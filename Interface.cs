df9acf43a55615766684cc78e3d1633192c385b7
@fanboi <bganti@st.ug.edu.gh>
2025-08-01 15:50:35 +0000

    Adding the Interface.cs file

---
+++ Interface.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+interface IMovable
+{
+    void Move();
+}
+
+class Car : IMovable
+{
+    public void Move()
+    {
+        Console.WriteLine("Car is moving");
+    }
+}
+
+class Bicycle : IMovable
+{
+    public void Move()
+    {
+        Console.WriteLine("Bicycle is moving");
+    }
+}
+
+class Interface
+{
+    static void Main(string[] args)
+    {
+        IMovable lamborghini = new Car();
+        IMovable electric_Bicycle = new Bicycle();
+
+        lamborghini.Move();      
+        electric_Bicycle.Move();  
+    }
+}
